7204cd0df9b2703c592a2f7c2cb904108749af75
Kenan Erkorkmaz <kenan.erkorkmaz@gmail.com>
2023-02-28 19:29:47 +0100

    test1 test2 olarak değiştirildi

--- src/Deneme1.java
+++ src/Deneme1.java
@@ -1,7 +1,7 @@
 public class Deneme1 {
 
     public static void main(String[] args) {
-        System.out.println("test1");
+        System.out.println("test2");
         System.out.println("");
     }
 }
